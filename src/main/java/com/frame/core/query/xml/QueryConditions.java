5d8d65cae2c3168ee50a27875ea776818f47707f
Defferson.Cheng <dcphone33@live.com>
2016-12-07 18:22:45 +0800

    略作修改 2016年12月7日18:22:31 自动查询好像可以使用

--- src/main/java/com/frame/core/query/xml/QueryConditions.java
+++ src/main/java/com/frame/core/query/xml/QueryConditions.java
@@ -1,6 +1,5 @@
 package com.frame.core.query.xml;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
--- src/main/webapp/resources/js/common/common-list.js
+++ src/main/webapp/resources/js/common/common-list.js
@@ -1,9 +1,44 @@
 /**
  * 
  */
+(function(){
+	window.submitQuery=function(){
+		var param={
+			page:$('#page').val(),
+			pageSize:$('#pageSize').val()
+		};
+		submitParams("","GET",param)
+	}
+})();
 $(document).ready(function(){
 	$('.data-panel>table').on("click","tbody>tr",function(e){
 		$(this).siblings().removeClass("selected");
 		$(this).addClass("selected");
+	});
+	$('gofrist:not(.disabled)').click(function(){
+		$('#page').val(1);
+		submitQuery();
+	});
+	$('goprev:not(.disabled)').click(function(){
+		$('#page').val($('#page').val()-1);
+		submitQuery();
+	});
+	$('input#jump').change(function(){
+		var page=$(this).val();
+		page=parseInt(page);
+		if (page>0&&page<=parseInt($('#totalPageCount').val())){
+			$('#page').val($(this).val());
+			submitQuery();
+		}else{
+			$(this).val($('#page').val());
+		}
+	});
+	$('gonext:not(.disabled)').click(function(){
+		$('#page').val(parseInt($('#page').val())+1);
+		submitQuery();
+	});
+	$('golast:not(.disabled)').click(function(){
+		$('#page').val($('#totalPageCount').val());
+		submitQuery();
 	});
 });
