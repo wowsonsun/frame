410ed914cb7f1a95a2666903d74b38f280ce9e26
Defferson.Cheng <dcphone33@live.com>
2016-12-17 23:15:44 +0800

    UI

--- src/main/java/com/frame/webapp/controller/BaseController.java
+++ src/main/java/com/frame/webapp/controller/BaseController.java
@@ -1,12 +1,18 @@
 package com.frame.webapp.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 /**
  * 这里面写异常处理的逻辑，
  * 1.进行一些通用的异常处理，比如SQL异常，乐观锁异常什么的。
@@ -25,5 +31,15 @@ public abstract class BaseController {
 		LOGGER.error("Control层捕获到异常。",e);
 		return null;
 	}
-	
+	SimpleDateFormat dateFormat1=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	SimpleDateFormat dateFormat2=new SimpleDateFormat("yyyy-MM-dd");
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		try {
+			binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat1, true));
+		} catch (Exception e) {
+			binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat2, true));
+		}
+		
+	}
 }
